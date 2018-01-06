5c8899de8dee25e9ad0481a3455f9a705a2ee8ad
YuriyDubov <31655754+YuriyDubov@users.noreply.github.com>
2018-01-06 11:31:43 +0300

    modified main.cpp BubbleSort start and info.txt

--- folder1/main.cpp
+++ folder1/main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
+const int ARRAY_SIZE = 10;
+void bubbleSort(int* arr){
+	//TODO
+}
 int main(int argc, char **argv){
-	cout << "Hello, World!" << endl;
+	int* arr = new arr[ARRAY_SIZE];
+	bubbleSort(&arr);
+	delete[] arr;
 	return 0;
 }

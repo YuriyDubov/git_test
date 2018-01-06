b65197c45542154ca92b64bc41fa3e9e7037e1d7
YuriyDubov <31655754+YuriyDubov@users.noreply.github.com>
2018-01-06 11:44:14 +0300

    bubble sort realization

--- folder1/main.cpp
+++ folder1/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
 using namespace std;
 const int ARRAY_SIZE = 10;
-void bubbleSort(int* arr){
-	//TODO
+void bubbleSort(int** arr){
+	bool end = false;
+	int temp;
+	for(int i = 0; i < ARRAY_SIZE - 1; i++){
+		end = true;
+		for(int j = ARRAY_SIZE - 1 - i; j > 0; j--)
+			if(*arr[j] < *arr[j - 1]){
+				end = false;
+				temp = *arr[j];
+				*arr[j] = *arr[j - 1];
+				*arr[j - 1] = temp;
+			}
+		if(end) i = ARRAY_SIZE;
+	}
 }
 int main(int argc, char **argv){
 	int* arr = new arr[ARRAY_SIZE];
